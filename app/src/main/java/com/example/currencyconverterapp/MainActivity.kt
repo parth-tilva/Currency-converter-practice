6472b799be05aac9776f98d5a64777094f8910f0
Parth Tilva <190305105281@paruluniversity.ac.in>
2022-05-12 18:03:32 +0530

    first commit

---
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.currencyconverterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.versionedparcelable.ParcelField
+import com.example.currencyconverterapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
